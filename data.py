7051f5d851dbe67af431103dd9c6870740bdf4af
jaerrib <jaerrib@tutanota.com>
2022-09-02 12:27:37 -0400

    Initial version of app with tkinter GUI

---
+++ data.py
@@ -0,0 +1,54 @@
+import requests
+from random import choice
+
+
+class Selector:
+
+    def __init__(self):
+        self.activity = ""
+        self.participants = "1"
+        self.price = "0.0"
+        self.accessibility = "0.0"
+
+    @staticmethod
+    def get_rand_type():
+        activity_types = [
+            "education",
+            "recreational",
+            "social",
+            "diy",
+            "charity",
+            "cooking",
+            "relaxation",
+            "music",
+            "busywork"]
+        return choice(activity_types)
+
+    @staticmethod
+    def get_rand_activity():
+        response = requests.get(
+            "http://www.boredapi.com/api/activity/")
+        response.raise_for_status()
+        data = response.json()
+        return data["activity"]
+
+    def get_suggestion(self, activity_type, participants, price, accessibility):
+
+        parameters = {
+            "type": activity_type,
+            "participants": participants,
+            "price": price,
+            "minaccessibility": "0",
+            "maxaccessibility": accessibility,
+        }
+
+        if activity_type == "random":
+            parameters["type"].set = self.get_rand_type()
+        response = requests.get(
+            "http://www.boredapi.com/api/activity/",
+            params=parameters)
+        response.raise_for_status()
+        data = response.json()
+        suggestion = data["activity"]
+        print(data, suggestion)
+        return suggestion
---
+++ main.py
@@ -0,0 +1,5 @@
+from data import Selector
+from ui import AppInterface
+
+selector = Selector()
+app_ui = AppInterface(selector)
